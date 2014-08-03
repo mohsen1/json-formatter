fbc6eb5862388cb2091b29f747bc531b8989855f
Mohsen Azimi <mazimi@apigee.com>
2014-08-03 13:26:50 -0700

    Use master for generating gh-pages

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,4 +1,22 @@
 'use strict';
 
-angular.module('jsonFormatterApp').controller('MainCtrl', function () {
+angular.module('jsonFormatterApp').controller('MainCtrl', function ($scope) {
+  $scope.complex = {
+    numbers: [
+      1,
+      2,
+      3
+    ],
+    boolean: true,
+    'null': null,
+    number: 123,
+    anObject: {
+      a: 'b',
+      c: 'd',
+      e: 'f\"'
+    },
+    string: 'Hello World'
+  };
+
+  $scope.deep = {a:{b:{c:{d:{}}}}};
 });
