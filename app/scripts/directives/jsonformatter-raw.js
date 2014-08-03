ce8dcdf4025f9f29764167a1b9d61e75eeb369fc
Mohsen Azimi <mazimi@apigee.com>
2014-08-03 14:41:22 -0700

    Tidy up

--- app/scripts/directives/jsonformatter-raw.js
+++ app/scripts/directives/jsonformatter-raw.js
@@ -12,27 +12,34 @@ angular.module('jsonFormatter', ['RecursionHelper']).directive('jsonFormatter', 
     if (scope.isObject) {
       scope.keys = Object.keys(scope.json);
     }
-
-    // If 'open' attribute is present
-    scope.isOpen = !!scope.open;
-
     scope.type = typeof scope.json;
     scope.hasKey = typeof scope.key !== 'undefined';
-
-    scope.escapeString = function (str) {
-      return str.replace('"', '\"');
-    };
-
+    scope.constructorName = scope.json && scope.json.constructor && scope.json.constructor.name;
     // Set custom type for null
     if (scope.json === null){
       scope.type = 'null';
     }
 
+    scope.escapeString = function (str) {
+      return str.replace('"', '\"');
+    };
+
     scope.isEmptyObject = function () {
       return scope.keys && !scope.keys.length && scope.isOpen && !scope.isArray();
     };
 
-    scope.constructorName = scope.json && scope.json.constructor && scope.json.constructor.name;
+
+    // If 'open' attribute is present
+    scope.isOpen = !!scope.open;
+    scope.toggleOpen = function () {
+      scope.isOpen = !scope.isOpen;
+    };
+    scope.childrenOpen = function () {
+      if (scope.open > 1){
+        return scope.open - 1;
+      }
+      return 0;
+    };
   }
 
   return {
